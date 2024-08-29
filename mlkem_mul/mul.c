4dfea6925f0698cb8ad03ca12a0da41db19f3093
Rod Chapman <rodchap@amazon.com>
2024-08-29 11:46:39 +0100

    Add second version mul2() with intermediate assertions
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- mlkem_mul/mul.c
+++ mlkem_mul/mul.c
@@ -30,8 +30,45 @@ uint32_t mul (uint32_t a, uint32_t b)
     return (uint32_t) r;
 }
 
+uint32_t mul2 (uint32_t a, uint32_t b)
+{
+    // As above, but using intermediate assertions
+    uint32_t m = a * b;
+    uint32_t m2;
+    int32_t  r;
+
+    m2 = (m * MAGIC) >> P;
+
+    // Owing to the error-bound on Magic, M2 is either
+    // "just right" or "too big by 1"
+    __CPROVER_assert (((m2 == m / Q) ||
+                       (m2 == (m / Q) + 1)), "assertion 1");
+
+    m2 = m2 * Q;
+
+    __CPROVER_assert (((m2 == (m / Q) * Q) ||
+                       (m2 == ((m / Q) * Q) + Q)), "assertion 2");
+
+    r = (int32_t) m - (int32_t) m2;
+
+    __CPROVER_assert (((r == (int32_t) m - (int32_t) ((m / Q) * Q)) ||
+                       (r == (int32_t) m - (int32_t) ((m / Q) * Q + Q))), "assertion 3");
+
+    r = r + (Q * (r < 0));
+
+    __CPROVER_assert (((uint32_t) r == m % Q), "assertion 4");
+
+    return (uint32_t) r;
+}
+
 void mul_harness()
 {
     uint32_t a, b, r;
     r = mul (a, b);
+}
+
+void mul2_harness()
+{
+    uint32_t a, b, r;
+    r = mul2 (a, b);
 }
--- mlkem_mul/mul.h
+++ mlkem_mul/mul.h
@@ -4,6 +4,7 @@
 #else
 #define __CPROVER_requires(a)
 #define __CPROVER_ensures(a)
+#define __CPROVER_assert(a)
 #endif
 
 #define Q 3329
@@ -13,6 +14,11 @@
 // is not permitted. Use of any "div" instruction in the generated
 // code is not permitted.
 uint32_t mul (uint32_t a, uint32_t b)
+__CPROVER_requires(a < Q)
+__CPROVER_requires(b < Q)
+__CPROVER_ensures(__CPROVER_return_value == (a * b) % Q);
+
+uint32_t mul2 (uint32_t a, uint32_t b)
 __CPROVER_requires(a < Q)
 __CPROVER_requires(b < Q)
 __CPROVER_ensures(__CPROVER_return_value == (a * b) % Q);
