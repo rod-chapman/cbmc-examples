426f81b8cc7220a355cfa28efe688ab2a2d1b2cd
Rod Chapman <rodchap@amazon.com>
2024-09-03 10:27:07 +0100

    Add implementation of Kyber Montgomery reduce
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- pqc/crypto.c
+++ pqc/crypto.c
@@ -70,6 +70,61 @@ uint16_t mod3329c (uint16_t a, uint16_t b)
 }
 
 
+#define QINV -3327 // q^-1 mod 2^16
+
+// This code from the Kyber reference implementation.
+int16_t mr1(int32_t a)
+{
+    int16_t t;
+
+    t = (int16_t)a * QINV;
+    t = (a - (int32_t)t * Q) >> 16;
+    return t;
+}
+
+
+// Alternative implementation, avoiding implementation-defined
+// casts and >> on a negative integer
+int16_t mr2(int32_t a)
+{
+    // -3327 -> uint16_t == -3327 + 65536 == 62209
+    const uint16_t UQINV = 62209;
+
+    // By default, CBMC is too strict about signed to unsigned conversion,
+    // which are well-defined according to C17 6.3.1.3(2)
+#pragma CPROVER check push
+#pragma CPROVER check disable "conversion"
+#pragma CPROVER check disable "signed-overflow"
+    // int32_t -> uint16_t - add or substract 65536 until in range
+    //  e.g. -1 -> 65535
+    uint16_t ua = (uint16_t) a;
+    uint32_t t1 = (uint32_t) (ua * UQINV);
+#pragma CPROVER check pop
+
+    t1 = t1 % 65536;
+
+    // t1 now in 0 .. 65535
+    // so conversion to int32_t is well-defined
+    int32_t t1s = (int32_t) t1;
+
+    t1s = t1s - (65536 * (t1s >= 32768));
+
+    // t1s now in -32768 .. 32767
+    int32_t t4 = t1s * Q;
+    int32_t t5 = a - t4;
+
+    __CPROVER_assert(t5 % 65536 == 0, "check t5 is a multiple of 65536");
+    // Therefore "/" is equivalent to >> for negative t5, but "/" is
+    // not implementation-defined
+    int32_t t6 = t5 / 65536;
+
+    // t6 now in -3328 .. 3328, therefore conversion to int16_t is well-defined
+    int16_t result = (int16_t) t6;
+
+    return result;
+}
+
+
 void mod3329_harness()
 {
     uint16_t x, y;
@@ -96,4 +151,18 @@ void mod3329c_harness()
     uint16_t x, y;
     uint16_t r;
     r = mod3329c(x, y);
+}
+
+void mr1_harness()
+{
+    int32_t a;
+    int16_t r;
+    r = mr1(a);
+}
+
+void mr2_harness()
+{
+    int32_t a;
+    int16_t r;
+    r = mr2(a);
 }
