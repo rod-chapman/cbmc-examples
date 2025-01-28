070983eb653c550320cc4b70dbe4c5cff1af7f11
Rod Chapman <rodchap@amazon.com>
2025-01-28 11:29:52 +0000

    Remove these examples
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- simple/simple.adb
+++
@@ -1,9 +0,0 @@
-package body Simple
-  with SPARK_Mode => On
-is
-   function Average (A, B : in Integer_32) return Integer_32
-   is
-   begin
-      return (A + B) / 2;
-   end Average;
-end Simple;
