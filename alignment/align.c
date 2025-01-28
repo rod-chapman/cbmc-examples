1ce529ba482c88804502e9e7de2fcc1bb2bf4c17
Rod Chapman <rodchap@amazon.com>
2025-01-28 09:27:58 +0000

    Update to use new CBMC macros
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- alignment/align.c
+++ alignment/align.c
@@ -1,39 +1,18 @@
-#include <stdint.h>
 #include <stdalign.h>
-
-#ifdef CBMC
-#else
-#define __CPROVER_assigns(...)
-#define __CPROVER_decreases(...)
-#define __CPROVER_assert(...)
-#define __CPROVER_requires(...)
-#define __CPROVER_ensures(...)
-#define __CPROVER_loop_invariant(...)
-#endif
+#include <stdint.h>
+#include "../common/cbmc.h"
 
 typedef uint64_t gf[4];
 
 void add(gf x)
-__CPROVER_requires(alignof(x) == 16)
-{
-    x[0] = x[0] + 1;
-}
-
+__contract__(
+  requires(alignof(x) == 16)
+) { x[0] = x[0] + 1; }
 
 int main()
 {
-    alignas(16) gf gf16 = { 1 };
-    add(gf16); // should be OK, but fails?
-
-//    alignas(8) gf gf8 = { 2 };
-//    add(gf8); // should fail
-
-//    gf gf_default = { 3 };
-//    add(gf_default); // should fail - alignment unknown
-
+  alignas(16) gf gf16 = {1};
+  add(gf16);  // should be OK, but fails?
 }
 
-void main_harness()
-{
-    int y = main();
-}
+void main_harness() { int y = main(); }
